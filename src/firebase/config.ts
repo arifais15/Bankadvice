73619f4bdac47f6422ea2ae84e4a9d0b66167d9a
Ariful Islam <Arif.AIS15@gmail.com>
2026-06-19 17:35:15 +0000

    look no console found

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,9 @@
 // This configuration is automatically updated by the Firebase Studio environment.
 export const firebaseConfig = {
   apiKey: "AIzaSy_PLACEHOLDER_KEY", // Note: The actual key is managed by the platform, but ensure the projectId is correct.
-  authDomain: "studio-3947565863.firebaseapp.com",
-  projectId: "studio-3947565863",
-  storageBucket: "studio-3947565863.appspot.com",
+  authDomain: "studio-9484611620-39f4f.firebaseapp.com",
+  projectId: "studio-9484611620-39f4f",
+  storageBucket: "studio-9484611620-39f4f.appspot.com",
   messagingSenderId: "123456789",
   appId: "1:123456789:web:abcdef123456"
 };
