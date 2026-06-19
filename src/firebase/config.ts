0592a380c3dbe5a8d557685dcb3adfc4feab7cf3
Ariful Islam <Arif.AIS15@gmail.com>
2026-06-19 17:15:47 +0000

    remove project link studio-9484611620

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,11 +1,10 @@
-// This configuration is updated to use your existing Firebase project.
-// You must go to the Firebase Console (https://console.firebase.google.com/) 
-// to retrieve your actual API Key and other configuration values.
+// This configuration needs to be updated with your actual Firebase project values.
+// You can find these in the Firebase Console: Project Settings > General > Your apps.
 export const firebaseConfig = {
   apiKey: "REPLACE_WITH_YOUR_ACTUAL_API_KEY",
-  authDomain: "studio-9484611620.firebaseapp.com",
-  projectId: "studio-9484611620",
-  storageBucket: "studio-9484611620.appspot.com",
+  authDomain: "REPLACE_WITH_PROJECT_ID.firebaseapp.com",
+  projectId: "REPLACE_WITH_PROJECT_ID",
+  storageBucket: "REPLACE_WITH_PROJECT_ID.appspot.com",
   messagingSenderId: "REPLACE_WITH_SENDER_ID",
   appId: "REPLACE_WITH_APP_ID"
 };
